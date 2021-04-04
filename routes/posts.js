d0ad0056ee3a27c0ca36a3a11e9fc8a98b6e43e5
jyotisingh301299 <jyotisingh301299@gmail.com>
2021-04-04 08:30:32 +0530

    displayed user and their corresponding post on home page

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const postsController = require('../controllers/posts_controller');
 
 
-router.post('/create-posts',postsController.create);
+router.post('/create-posts',passport.checkAuthentication, postsController.create);
 
 module.exports = router;

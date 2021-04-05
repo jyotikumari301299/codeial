0ce1857e09c71c706206b5ebef2607da8eaa3691
jyotisingh301299 <jyotisingh301299@gmail.com>
2021-04-05 14:17:54 +0530

    deleting a post and all the comments related to that by  authorized users

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,12 +1,31 @@
-const  post = require("../models/post");
+const  Post = require("../models/post");
+const Comment = require("../models/comment");
 
 // inserting posts into DB
 module.exports.create = (req,res)=>{
-    post.create({
+    Post.create({
         content: req.body.content,
         user: req.user._id
     },function(err,post){
         if(err){ console.log("error in creating a post"); return; }
         return res.redirect('back');
+    })
+}
+
+
+module.exports.destroy = (req,res)=>{
+    Post.findById(req.params.id,(err,post)=>{
+        // .id means converting the object id into string
+        if(post.user == req.user.id){
+            post.remove();
+// when we delete a post we have to delete all the comments related to that particular post
+            Comment.deleteMany({post: req.params.id},(err)=>{
+                return res.redirect('back');
+            })
+        }
+        else
+        {
+            return res.redirect('back');
+        }
     })
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 // index.js is my entry point to all the routes
 
+
+// aapne push kia hai khi pe use?han cooments ko push kia h
+
 const express = require('express');
 const router = express.Router();
 // we need to require controller first before using it

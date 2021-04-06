afbc58e534b5dd471257743909f503032bc81b55
jyotisingh301299 <jyotisingh301299@gmail.com>
2021-04-07 03:11:50 +0530

    converting more code to Async Await

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,45 +1,71 @@
-const  Comment = require("../models/comment");
+const Comment = require("../models/comment");
 const Post = require("../models/post");
 
-module.exports.create = function(req,res){
-    Post.findById(req.body.post, function(err,post){
-        if(err){ console.log("post doesn't exists"); return res.redirect('back'); }
-        if(post)
-        {
-            console.log("in side create comment");
-            Comment.create({
-                content: req.body.content,
-                post: req.body.post,
-                user: req.user._id
-            }, function(err,comment){
-                if(err){
-                    console.log("error in creating comment");
-                    return res.redirect('back');
-                }
-                // push function is given by mongodb...it will automatically fetch the id and push it
-                post.comments.push(comment);
-                // whenever we update anything we need to call save after this
-                post.save();
-                return res.redirect('/');
-            });
-            
+module.exports.create = async function (req, res) {
+  try {
+    let post = await Post.findById(req.body.post);
+    if (post) {
+      let comment = await Comment.create({
+        content: req.body.content,
+        post: req.body.post,
+        user: req.user._id,
+      });
+      // push function is given by mongodb...it will automatically fetch the id and push it
+      post.comments.push(comment);
+      // whenever we update anything we need to call save after this
+      post.save();
+      return res.redirect("/");
+    }
+  } catch (err) {
+    console.log("Error", err);
+    return;
+  }
+};
+
+// module.exports.create = function(req,res){
+//     Post.findById(req.body.post, function(err,post){
+//         if(err){ console.log("post doesn't exists"); return res.redirect('back'); }
+//         if(post)
+//         {
+//             console.log("in side create comment");
+//             Comment.create({
+//                 content: req.body.content,
+//                 post: req.body.post,
+//                 user: req.user._id
+//             }, function(err,comment){
+//                 if(err){
+//                     console.log("error in creating comment");
+//                     return res.redirect('back');
+//                 }
+//                 // push function is given by mongodb...it will automatically fetch the id and push it
+//                 post.comments.push(comment);
+//                 // whenever we update anything we need to call save after this
+//                 post.save();
+//                 return res.redirect('/');
+//             });
+
+//         }
+//     })
+// }
+
+module.exports.destroy = (req, res) => {
+  Comment.findById(req.params.id, (err, comment) => {
+    if (err) {
+      console.log("eror in deleting comments");
+      return res.redirect("back");
+    }
+    if (comment.user == req.user.id) {
+      let postId = comment.post;
+      comment.remove();
+      Post.findByIdAndUpdate(
+        postId,
+        { $pull: { comments: req.params.id } },
+        (err, post) => {
+          return res.redirect("back");
         }
-    })
-}
-
-
-module.exports.destroy = (req,res)=>{
-    Comment.findById(req.params.id,(err,comment)=>{
-        if(err){ console.log("eror in deleting comments"); return res.redirect('back');  }
-        if(comment.user == req.user.id)
-        {
-            let postId = comment.post;
-            comment.remove();
-            Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}}, (err,post)=>{
-                return res.redirect('back');
-            })
-        }else {
-        return res.redirect('back');}
-    })
-}
-
+      );
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,31 +1,60 @@
-const  Post = require("../models/post");
+const Post = require("../models/post");
 const Comment = require("../models/comment");
 
 // inserting posts into DB
-module.exports.create = (req,res)=>{
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    },function(err,post){
-        if(err){ console.log("error in creating a post"); return; }
-        return res.redirect('back');
-    })
-}
+// module.exports.create = (req,res)=>{
+//     Post.create({
+//         content: req.body.content,
+//         user: req.user._id
+//     },function(err,post){
+//         if(err){ console.log("error in creating a post"); return; }
+//         return res.redirect('back');
+//     })
+// }
+module.exports.create = async (req, res) => {
+  try {
+    await Post.create({
+      content: req.body.content,
+      user: req.user._id,
+    });
+    return res.redirect("back");
+  } catch (err) {
+    console.log("Error", err);
+    return;
+  }
+};
 
+// module.exports.destroy = (req, res) => {
+//   Post.findById(req.params.id, (err, post) => {
+//     // .id means converting the object id into string
+//     if (post.user == req.user.id) {
+//       post.remove();
+//       // when we delete a post we have to delete all the comments related to that particular post
+//       Comment.deleteMany({ post: req.params.id }, (err) => {
+//         return res.redirect("back");
+//       });
+//     } else {
+//       return res.redirect("back");
+//     }
+//   });
+// };
 
-module.exports.destroy = (req,res)=>{
-    Post.findById(req.params.id,(err,post)=>{
-        // .id means converting the object id into string
-        if(post.user == req.user.id){
-            post.remove();
-// when we delete a post we have to delete all the comments related to that particular post
-            Comment.deleteMany({post: req.params.id},(err)=>{
-                return res.redirect('back');
-            })
-        }
-        else
-        {
-            return res.redirect('back');
-        }
-    })
-}
+module.exports.destroy = async (req, res) => {
+    try{
+        let post = await Post.findById(req.params.id);
+      // .id means converting the object id into string
+       if (post.user == req.user.id) {
+        post.remove();
+        // when we delete a post we have to delete all the comments related to that particular post
+        Comment.deleteMany({ post: req.params.id });
+          return res.redirect("back");
+       }
+      else {
+        return res.redirect("back");
+      }
+    }
+      catch(err){
+          console.log("Error",err);
+          return;
+      }
+    }

2becdf705d8685258ab360f59ffd320fd304721a
jyotisingh301299 <jyotisingh301299@gmail.com>
2021-04-07 02:26:49 +0530

    converting to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -14,7 +14,7 @@ module.exports.home = async function (req, res) {
       });
 
     let users =  await User.find({});
-
+// mai wawait lgana bhul gyi this tat was important
     return res.render("home", {
       title: "Codeial | Home",
       posts: posts,
